19af5a40f377ecf58bbd41b3d8f5b31debb13c19
scotow <contact@scotow.com>
2022-04-15 00:23:10 +0200

    Initial implementation

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::env::args;
+use std::io::Stdout;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn main() {
+    let args = args().skip(1).collect::<Vec<_>>();
+    assert!(args.len() >= 2);
+
+    let stacks_dir = Path::new(&args[0]);
+    assert!(stacks_dir.is_dir());
+
+    for image in &args[1..] {
+        let project = image.split("/").next().unwrap();
+        let config_file = find_config_file(stacks_dir, project).unwrap();
+
+        Command::new("docker-compose")
+            .args(["-p", project, "-f", config_file.to_str().unwrap(), "pull"])
+            .spawn()
+            .unwrap();
+        Command::new("docker-compose")
+            .args([
+                "-p",
+                project,
+                "-f",
+                config_file.to_str().unwrap(),
+                "up",
+                "-d",
+            ])
+            .spawn()
+            .unwrap();
+    }
+}
+
+fn find_config_file(dir: &Path, project: &str) -> Option<PathBuf> {
+    let yml = dir.join(format!("{}.yml", project));
+    if yml.is_file() {
+        return Some(yml);
+    }
+    let yaml = dir.join(format!("{}.yaml", project));
+    if yaml.is_file() {
+        return Some(yaml);
+    }
+    None
+}
